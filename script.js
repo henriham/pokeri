fe6b5a048c7421d5c59a21b7754d9be6787b974f
henriham <112693278+henriham@users.noreply.github.com>
2024-11-24 20:41:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -105,163 +105,70 @@ betBtn.addEventListener('click', () =>{
 let hand = [];
 let allCards = [];
 
-
-async function initCards() {
+async function initCards() {    
     const response = await fetch('cards.php');
     allCards = await response.json();
-    
+
     while(hand.length < 5){
         let random = Math.floor(Math.random() * allCards.length);
         let pickedCard = allCards.splice(random, 1)[0];
         if(pickedCard){
             hand.push(pickedCard);
-        }
-        
+        }        
     }
-    
 }
 
-
 document.querySelector('.dealBtn').addEventListener('click', firstDeal);
+
 async function firstDeal() {
-    if(allCards.length === 0){
-        await initCards();
-    }
-    let displayedCards = document.querySelectorAll('.card');
-    let h1El = document.querySelector('h1');
-    displayedCards.forEach((card, index) => {
-        if(h1El){
-            h1El.remove();
+    if(!firstDealCompleted){   
+        if(allCards.length === 0){
+            await initCards();
         }
+        let displayedCards = document.querySelectorAll('.card');
+        let h1El = document.querySelector('h1');
+        displayedCards.forEach((card, index) => {
+            if(h1El){
+                h1El.remove();
+            }
         card.querySelector('img').src = `./media/cards/${hand[index]}.svg`;
-    })
-    console.log(allCards)
-    console.log(hand)
-    
+        })
+        console.log(allCards);
+        console.log(hand);
+        firstDealCompleted = true;
+    }
 }
 
 const displayedCards = document.querySelectorAll('.card');
+let firstDealCompleted = false;
+let selectedCards = [];
+
 function cardSelector(event){
-    console.log("card clicked ", event.currentTarget);
+    
     const card = event.currentTarget;
+    const index = Array.from(displayedCards).indexOf(card);
     let h1El = card.querySelector('h1');
+    if(!firstDealCompleted){
+        console.log("cards have not been delt yet !");
+        return;
+    }    
     if(!h1El){
         const h1 = document.createElement('h1');
         h1.textContent = 'LUKITTU';
         h1.className = 'selected';
         card.appendChild(h1); 
+        selectedCards.push({ index, value: hand[index]});
+        console.log(`Selected: ${JSON.stringify(selectedCards)}`);
     }else{
         h1El.remove();
-    }
-    
+        const cardRemove = selectedCards.find(card => card.index === index);
+        selectedCards.splice(selectedCards.indexOf(cardRemove), 1);
+        console.log(`card removed, Remaining; ${JSON.stringify(selectedCards)}`);
+        
+    } 
     
 }
 displayedCards.forEach((card) => {
     card.addEventListener('click', cardSelector)
-})
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Bet and wintable component  end
-
-
-
-// Cards start
-/* let allCards = [];
-let hand = [];
-let selectedCards = [];
-
-async function initCards(){
-    const response = await fetch('cards.php');
-    allCards = await response.json();
-    hand = [];
-    selectedCards = [];
-    
-}
-
-async function displayHand(){
-    if(allCards.length === 0){
-        await initCards();
-    }    
-    if(hand.length === 0){
-        hand = getRandomCards(5);
-        
-    
-    }else{
-        let newCardsNeeded = 5 - selectedCards.length;
-        let newCards = getRandomCards(newCardsNeeded);
-
-        hand = hand.map((card, index) => {
-            return selectedCards.includes(index) ? card : newCards.shift();
-        });
-    }
-
-    let displayedCards = document.querySelectorAll('.card');
-    displayedCards.forEach((card, index) => {
-        const h1El = document.querySelector('h1');
-        if(h1El){
-            h1El.remove();
-        }
-        card.querySelector('img').src = `./media/cards/${hand[index]}.svg`;
-    });
-    cardSelectHandler();
-    
-
-}
-
-function getRandomCards(num){
-    const newCards = [];
-    while(newCards.length < num){
-        let random = Math.floor(Math.random() * allCards.length);
-        let pickedCard = allCards.splice(random, 1)[0];
-        if(pickedCard){
-            newCards.push(pickedCard);
-        }
-    }
-    console.log("newCards "+ newCards)
-    return newCards;
-}
-
-function cardSelectHandler(){
-    const displayedCards = document.querySelectorAll('.card');
-    displayedCards.forEach((card, index) => {
-        const h1El = document.querySelector('h1');
-        if(h1El){
-            h1El.remove();    
-        }
-            const h1 = document.createElement('h1');
-            h1.textContent = 'LUKITTU';
-            h1.className = '.selected';
-            card.appendChild(h1);
-            selectedCards.push(index);
-        
-        console.log(`Selected Cards: ${selectedCards}`);
-    })
-}
-
-
-
-document.querySelector('.dealBtn').addEventListener('click', displayHand)
-initCards()
-
-
-
-
-
-
- */
-// Cards end
